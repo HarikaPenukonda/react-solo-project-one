f399c44b4f5713dfd2dd3b25f74cac5ff55ee3bd
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-10 15:26:08 -0500

    removed card and added to app.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import profile from "../images/profile-logo.jpg"
 
 export default function MainContent(){
     return(
-        <div  className="card">
+        <main>
             <img src={profile}
                 alt="Harika Penukonda"
                 className="pic"
@@ -12,17 +12,22 @@ export default function MainContent(){
             <h5 className="card--position">Full Stack Java Developer</h5>
             <p className="card--website">harikapenukonda.website</p>
             
-            <button className="fas fa-envelope-square">Email</button>
-            <button className="fa fa-linkedin">LinkedIn</button>
-            <h3>About</h3>
-            <p>
-                I am a full stack developer with over 5 years of experience, 
-                who loved to code and enjoys new challenges.
-            </p>
-            <h3>My Interests</h3>
-            <p>
-                Reader, Mural Arts, Yoga, Carnatic Music, Traveller
-            </p>
-        </div>
+            
+                <button className="button button-email">Email</button>
+                <button className="button button-linkedin">LinkedIn</button>
+            {/* <button className="fas fa-envelope-square">Email</button>
+            <button className="fas fa-linkedin">LinkedIn</button> */}
+            <h3 className="about--title">About</h3>
+                <p className="p1">
+                    I am a full stack developer with over 5 years of experience, 
+                    who loved to code and enjoys new challenges.
+                </p>
+            <h3 className="interest--title">Interests</h3>
+                <p className="p2">
+                    Reader, Mural Arts, Yoga, Carnatic Music, Traveller
+                </p>  
+        
+            
+        </main>
     )
 }
